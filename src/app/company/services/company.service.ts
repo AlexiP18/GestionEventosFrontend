d4dd2d34d71ed33eab46fdf02fe7c8130d68efe7
Alexis Poaquiza <apoaquiza6287@uta.edu.ec>
2024-04-25 15:29:53 -0500

    Se creo el servicio company y se modifico para poder realizar un POST

---
+++ src/app/company/services/company.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserStoargeService } from '../../basic/services/storage/user-storage.service';
+
+const BASIC_URL = "http://localhost:8080/";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompanyService {
+
+  constructor(private http: HttpClient) { }
+
+  postAd(adDTO:any): Observable<any>{
+    const userId = UserStoargeService.getUserId();
+    return this.http.post(BASIC_URL + `api/company/ad/${userId}`, adDTO, {
+      headers : this.createAuthorizationHeader()
+    })
+  }
+
+  createAuthorizationHeader(): HttpHeaders{
+    let authHeaders: HttpHeaders = new HttpHeaders();
+    return authHeaders.set(
+      'Authorization',
+      'Bearer ' + UserStoargeService.getToken()
+    )
+  }
+
+}
