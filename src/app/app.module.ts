90394a13b2b75940f4e0e9e787d5ecc9d0030824
dalembertbravo <dalembertbravo2@gmail.com>
2024-04-24 20:11:59 -0500

    Cree los servicios de los registros y de paso cree el componente signupCliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemoNgZorroAntdModule } from './DemoNgZorroAntdModule';
+import { SignupClientComponent } from './basic/components/signup-client/signup-client.component';
 
 registerLocaleData(en);
 
@@ -21,7 +22,8 @@ registerLocaleData(en);
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    SignupClientComponent
   ],
   imports: [
     BrowserModule,
--- src/app/basic/services/auth/auth.service.ts
+++ src/app/basic/services/auth/auth.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+const BASIC_URL = 'http://localhost:8080/';
+export const AUTH_HEADER = 'authorization';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  //registrar el cliente
+  registerClient(signupRequestDTO: any): Observable<any> {
+    return this.http.post(BASIC_URL + 'client/sign-up', signupRequestDTO);
+  }
+  //registramos el equipo
+  registerCompany(signupRequestDTO: any): Observable<any> {
+    return this.http.post(BASIC_URL + 'company/sign-up', signupRequestDTO);
+  }
 }
