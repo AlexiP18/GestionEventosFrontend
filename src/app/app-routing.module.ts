81e4791a2bda518d1474a68bf49d92e84055b1c4
dalembertbravo <dalembertbravo2@gmail.com>
2024-04-24 20:47:20 -0500

    cree el modulo de SignupCompany donde alojaría el formularios de equipos, copie el mismo formulario de usuarios cambiando solo por direccion.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupClientComponent } from './basic/components/signup-client/signup-client.component';
+import { SignupCompanyComponent } from './basic/components/signup-company/signup-company.component';
 
 const routes: Routes = [
   { path: 'register_client', component: SignupClientComponent },
+  { path: 'register_company', component: SignupCompanyComponent },
   {
     path: 'company',
     loadChildren: () =>
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemoNgZorroAntdModule } from './DemoNgZorroAntdModule';
 import { SignupClientComponent } from './basic/components/signup-client/signup-client.component';
+import { SignupCompanyComponent } from './basic/components/signup-company/signup-company.component';
 
 registerLocaleData(en);
 
@@ -23,7 +24,8 @@ registerLocaleData(en);
     AppComponent,
     LoginComponent,
     SignupComponent,
-    SignupClientComponent
+    SignupClientComponent,
+    SignupCompanyComponent
   ],
   imports: [
     BrowserModule,
