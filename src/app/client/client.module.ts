0b875cf907d0bcc67d7aacad2fd7bc47467613e5
Alexis Poaquiza <apoaquiza6287@uta.edu.ec>
2024-04-25 16:49:01 -0500

    Se modifico el componente Dashboard del modulo de clientes, y s modifico el servicio

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -4,16 +4,21 @@ import { CommonModule } from '@angular/common';
 import { ClientRoutingModule } from './client-routing.module';
 import { ClientComponent } from './client.component';
 import { ClientDashboardComponent } from './pages/client-dashboard/client-dashboard.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DemoNgZorroAntdModule } from '../DemoNgZorroAntdModule';
 
 
 @NgModule({
   declarations: [
     ClientComponent,
-    ClientDashboardComponent
+    ClientDashboardComponent,
   ],
   imports: [
     CommonModule,
-    ClientRoutingModule
+    ClientRoutingModule,
+    DemoNgZorroAntdModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ClientModule { }
